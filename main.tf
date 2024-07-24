d655205522546c0a70b274ff7139a3647d52bb06
rajaak85 <akrajiak5@gmail.com>
2024-07-24 16:11:48 +0530

    C1

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    access_key = "AKIAW5IZRWOYYMO4M2EZ"
+    secret_key = "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"
+    region =  "us-east-1"
+}
+
+module "myres" {
+  source = "./demo_mod1"
+  var_cidr = "10.0.0.0/16"
+  var_vpcname = "vpc_module_27"
+  var_bktname = "bucketmodule27"
+}
+
